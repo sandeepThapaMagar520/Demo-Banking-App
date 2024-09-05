162c247200b1455535706ab27ff273f1a43f1ee0
sandeepThapaMagar510 <sandeepthapa520@gmail.com>
2024-09-05 15:39:35 +0545

    Initial commit

---
+++ src/main/java/com/learningSpring/demo_banking_app/DemoBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.learningSpring.demo_banking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBankingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningSpring/demo_banking_app/DemoBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpring.demo_banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
